9712722bebef33b5a710c3e220fdc246438f4c46
rainrun <33125415+rainrun@users.noreply.github.com>
2018-09-17 18:27:37 +0800

    修改application

--- src/main/java/cn/edu/bupt/Application.java
+++ src/main/java/cn/edu/bupt/Application.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.PropertySource;
  * Created by Administrator on 2018/4/10.
  */
 @SpringBootApplication
-/*@ComponentScan("cn.edu.bupt")
+@ComponentScan("cn.edu.bupt")
 @MapperScan("cn.edu.bupt.mapper")
 @PropertySource({"classpath:disconf.properties"})
-@ImportResource({"classpath:disconf.xml"})//引入disconf*/
+@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
